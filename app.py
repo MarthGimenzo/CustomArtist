73e0e4b3632cff4afc9025e1f0d853fd0d078e23
Jouke de Vries <joukev87@gmail.com>
2020-07-20 15:11:08 +0000

    Deleted secret data in order to create environment variables

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'custom_artist'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-rx4bk.mongodb.net/custom_artist?retryWrites=true&w=majority'
-
 @app.route('/')
 def hello():
     return 'Hello World'
