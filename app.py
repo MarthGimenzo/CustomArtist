93299483eaac8fddf6924db75ec73abeca13fea7
Jouke de Vries <joukev87@gmail.com>
2020-07-20 15:09:32 +0000

    Imported PyMongo and ObjectId

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'custom_artist'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-rx4bk.mongodb.net/custom_artist?retryWrites=true&w=majority'
 
 @app.route('/')
 def hello():
