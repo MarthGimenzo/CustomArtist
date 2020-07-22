094eb951a9e84b5213a7e74a91390334d17b57b2
Jouke de Vries <joukev87@gmail.com>
2020-07-22 15:57:24 +0000

    Starting background image is now decided randomly

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,32 @@
-$(document).ready(function(){
-  
-    // Fading Backgrounds
-    
-    // Credits to rudolfson.junior for his tutorial on Youtube
-    // https://www.youtube.com/watch?v=XjPdQSqtJhs
-    // Thank you for the great explanation!
+$(document).ready(function () {
+  // Fading Backgrounds
 
-var count = 2;
-var images = ["../static/images/backgrounds/bg1.jpg",
-            "../static/images/backgrounds/bg2.jpg",
-            "../static/images/backgrounds/bg3.jpg",
-            "../static/images/backgrounds/bg4.jpg"];
+  // Credits to rudolfson.junior for his tutorial on Youtube
+  // https://www.youtube.com/watch?v=XjPdQSqtJhs
+  // Thank you for the great explanation!
 
+  var count = Math.floor(Math.random() * 4);
+  console.log(count);
+  var images = [
+    "../static/images/backgrounds/bg1.jpg",
+    "../static/images/backgrounds/bg2.jpg",
+    "../static/images/backgrounds/bg3.jpg",
+    "../static/images/backgrounds/bg4.jpg",
+  ];
 
-var image = $(".fader")
+  var image = $(".fader");
+  image.css("background-image", "url(" + images[count] + ")");
+  count++;
 
-
-  setInterval(function(){
-    image.fadeOut(1000, function(){
-      image.css("background-image","url("+images[count++]+")");
+  setInterval(function () {
+    image.fadeOut(1000, function () {
+      console.log(count);
+      image.css("background-image", "url(" + images[count++] + ")");
+      console.log(count);
       image.fadeIn(1000);
-      
     });
-    if(count == images.length)
-    {
+    if (count == images.length) {
       count = 0;
     }
-  },10000);
-
-
-
+  }, 10000);
 });
