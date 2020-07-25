0d649236328bbb46b8f2d735b7f06a4c2bec4682
Jouke de Vries <joukev87@gmail.com>
2020-07-25 16:42:47 +0000

    Set up basic build for login system, fixed bug that defined app.config['MONGO_DBNAME'] and app.config['MONGO_URI'] incorrectly. Updated requirements.txt. Added register_artist.html

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+import bcrypt
+
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-MONGO_CA = os.environ['MONGO_CA']
-MONGO_URI_CA = os.environ['MONGO_URI_CA']
+app.config['MONGO_DBNAME'] = os.environ['MONGO_CA']
+app.config['MONGO_URI'] = os.environ['MONGO_URI_CA']
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return 'You are a banana'
+
+@app.route('/register_artist', methods=['POST', 'GET'])
+def register_artist():
+    if request.method == 'POST':
+        artists = mongo.db.artists
+        existing_artist = artists.find_one({'username' : request.form['username']})
+
+        if existing_artist == None:
+            hashpass = bcrypt.hashpw(request.form['pass'], bcrypt.gensalt())
+            artists.insert({'name' : request.form['username'], password : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+    
+    print('Hello, world!')
+    return render_template('register_artist.html')
+
+    return 'You are a mango' 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
