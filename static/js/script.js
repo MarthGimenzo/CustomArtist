636e9b6926c5cc00d7ad82d08cfdca2934c6d5b3
Jouke de Vries <joukev87@gmail.com>
2020-07-29 17:23:20 +0000

    Added a randomizer to background images. Last picture is never chosen immediately after it.

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,42 @@
-$(document).ready(function () {
-  // Fading Backgrounds
+$(document).ready(function() {
+    // Fading Backgrounds
 
-  // Credits to rudolfson.junior for his tutorial on Youtube
-  // https://www.youtube.com/watch?v=XjPdQSqtJhs
-  // Thank you for the great explanation!
+    // Credits to rudolfson.junior for his tutorial on Youtube
+    // https://www.youtube.com/watch?v=XjPdQSqtJhs
+    // Thank you for the great explanation!
 
-  var count = Math.floor(Math.random() * 3);
-  console.log(count);
-  var images = [
-    "../static/images/backgrounds/bg1.jpg",
-    "../static/images/backgrounds/bg2.jpg",
-    "../static/images/backgrounds/bg3.jpg",
-    "../static/images/backgrounds/bg4.jpg",
-  ];
+    var count = Math.floor(Math.random() * 3);
 
-  var image = $(".fader");
-  image.css("background-image", "url(" + images[count] + ")");
-  count++;
 
-  setInterval(function () {
-    image.fadeOut(1000, function () {
-      console.log(count);
-      image.css("background-image", "url(" + images[count++] + ")");
-      console.log(count);
-      image.fadeIn(1000);
-    });
-    if (count == images.length) {
-      count = 0;
-    }
-  }, 10000);
+    console.log('count at first: ' + count);
+    var images = [
+        "../static/images/backgrounds/bg1.jpg",
+        "../static/images/backgrounds/bg2.jpg",
+        "../static/images/backgrounds/bg3.jpg",
+        "../static/images/backgrounds/bg4.jpg",
+    ];
+
+    var image = $(".fader");
+    image.css("background-image", "url(" + images[count] + ")");
+    console.log('count now: ' + count);
+
+    setInterval(function() {
+        
+
+        image.fadeOut(1000, function() {
+
+            console.log('old count: ' + count);
+            var oldcount = count;
+            while (count == oldcount){
+                count = Math.floor(Math.random() * 4);
+            }
+            
+
+            // New picture presents defined on new count
+            image.css("background-image", "url(" + images[count] + ")");
+            console.log('new picture presents: ' + count);
+            image.fadeIn(1000);
+        });
+        
+    }, 10000);
 });
